411515b81287b8de8b38d3acae91b9d4d7fa6a37
Austin-Earl-Private <theqmind2020@gmail.com>
2022-01-12 19:09:17 -0700

    starting

--- project03-02/models/product.js
+++ project03-02/models/product.js
@@ -15,7 +15,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
@@ -23,10 +24,30 @@ module.exports = class Product {
     }
 
     save() {
-        this.id = Math.random().toString();
         getProductsFromFile((products) => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+            if (this.id) {
+                const exsitingProductIndex = products.findIndex(
+                    (prod) => prod.id === this.id
+                );
+                const updatedProducts = [...products];
+                updatedProducts[exsitingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log(err);
+                });
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                });
+            }
+        });
+    }
+
+    static deleteById(prodId) {
+        getProductsFromFile((products) => {
+            const keepProducts = products.filter((prod) => prod.id !== prodId);
+            fs.writeFile(p, JSON.stringify(keepProducts), (err) => {
                 console.log(err);
             });
         });
