f1ca7395148f11ac2cdd5e810ba85920c285b183
Austin-Earl-Private <theqmind2020@gmail.com>
2022-01-13 19:56:56 -0700

    normalized layout

--- project03/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAddProduct, postAddProduct } = require('../controllers/products');
-
-const router = express.Router();
-router.get('/add-product', getAddProduct);
-
-router.post('/add-product', postAddProduct);
-
-exports.routes = router;
